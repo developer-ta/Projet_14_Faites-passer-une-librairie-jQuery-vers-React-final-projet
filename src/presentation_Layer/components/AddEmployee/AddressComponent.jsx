8a5f8e4ca5d3c70d1f96be9c71d133b957970d5a
tayier <ntparis9@gmail.com>
2025-04-04 01:35:47 +0200

    add scss media screen

--- src/presentation_Layer/components/AddEmployee/AddressComponent.jsx
+++ src/presentation_Layer/components/AddEmployee/AddressComponent.jsx
@@ -6,14 +6,7 @@ import 'dayjs/locale/fr';
 import { Autocomplete, Box, Button, Container, InputBase, TextField } from '@mui/material';
 import { FormControl, InputLabel, Input } from '@mui/material';
 
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'The Godfather', year: 1972 },
-  { label: 'The Godfather: Part II', year: 1974 },
-  { label: 'The Dark Knight', year: 2008 },
-  { label: '12 Angry Men', year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-];
+
 const states = [
   {
     label: 'Alabama',
