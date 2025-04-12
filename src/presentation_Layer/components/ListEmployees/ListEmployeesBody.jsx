8a159bc3d2f18cb55d8abddbdf570c4b30fa4215
tayier <ntparis9@gmail.com>
2025-04-12 22:42:57 +0200

    fixed date filter error

--- src/presentation_Layer/components/ListEmployees/ListEmployeesBody.jsx
+++ src/presentation_Layer/components/ListEmployees/ListEmployeesBody.jsx
@@ -5,103 +5,113 @@ import { useCallback } from 'react';
 import { Box, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-const columns = [
-  // { field: 'id', headerName: 'index', width:70, headerAlign: 'center-header', type: 'number' },
-  // { field: 'id', headerName: 'Id', type: 'number' },
-  { field: 'firstName', headerName: 'First name', type: 'string', width: 100 },
-  { field: 'lastName', headerName: 'Last name', type: 'string', width: 100 },
-  {
-    field: 'startDate',
-    headerName: 'Start Date',
-    type: 'number',
-    width: 100,
-  },
-  {
-    field: 'department',
-    headerName: 'Department',
-    type: 'string',
-    width: 100,
-  },
-  {
-    field: 'dateOfBirth',
-    headerName: 'Date of Birth',
-    type: 'number',
-    width: 100,
-  },
 
-  {
-    field: 'street',
-    headerName: 'Street',
-    type: 'number',
-    width: 100,
-  },
-  {
-    field: 'city',
-    headerName: 'City',
-    type: 'string',
-    width: 100,
-  },
-  {
-    field: 'state',
-    headerName: 'State',
-    type: 'number',
-    width: 100,
-    // width: 90,
-  },
-  {
-    field: 'zipCode',
-    headerName: 'Zip Code',
-    type: 'number',
-    width: 100,
-  },
-];
+ function dateAdapter(dateFr) {
+   if (dateFr) {
+     const [d, m, y] = dateFr.split('/');
+     const valideDate = new Date(y, m - 1, d);
+     
+     return valideDate;
+   }
+   return null;
+ }
+ const columns = [
+   // { field: 'id', headerName: 'index', width:70, headerAlign: 'center-header', type: 'number' },
+   // { field: 'id', headerName: 'Id', type: 'number' },
+   { field: 'firstName', headerName: 'First name', type: 'string', width: 100 },
+   { field: 'lastName', headerName: 'Last name', type: 'string', width: 100 },
+   {
+     field: 'startDate',
+     headerName: 'Start Date',
+     type: 'date',
+     valueGetter: (d) => dateAdapter(d),
+     width: 100,
+   },
+   {
+     field: 'department',
+     headerName: 'Department',
+     type: 'string',
+     width: 100,
+   },
+   {
+     field: 'dateOfBirth',
+     headerName: 'Date of Birth',
+     type: 'date',
+     valueGetter: (date) => dateAdapter(date),
+     width: 100,
+   },
 
-export default function DataTable() {
+   {
+     field: 'street',
+     headerName: 'Street',
+     type: 'number',
+     width: 100,
+   },
+   {
+     field: 'city',
+     headerName: 'City',
+     type: 'string',
+     width: 100,
+   },
+   {
+     field: 'state',
+     headerName: 'State',
+     type: 'string',
+     width: 100,
+     // width: 90,
+   },
+   {
+     field: 'zipCode',
+     headerName: 'Zip Code',
+     type: 'number',
+     width: 100,
+   },
+ ];
 
-  const { employeeProfile } = useSelector((state) => state.employeeProfileReducer);
+ export default function DataTable() {
+   const { employeeProfile } = useSelector((state) => state.employeeProfileReducer);
 
-  const rowsConstructor = useCallback(
-    (list) =>
-      list.map((el, index) => {
-        return { ...el, id: `${index}-${el.firstName}-${el.lastName}-(${el.state})` };
-      }),
-    []
-  );
+   const rowsConstructor = useCallback(
+     (list) =>
+       list.map((el, index) => {
+         return { ...el, id: `${index}-${el.firstName}-${el.lastName}-(${el.dateOfBirth})` };
+       }),
+     []
+   );
 
+   return (
+     <main className="container">
+       {' '}
+       <Box sx={{ height: 500, width: '95%' }}>
+         <Typography variant="h3" component="h3" sx={{ textAlign: 'center', mt: 3, mb: 3 }}>
+           Current Employees
+         </Typography>
 
-  return (
-    <main className="container">
-      {' '}
-      <Box sx={{ height: 500, width: '95%' }}>
-        <Typography variant="h3" component="h3" sx={{ textAlign: 'center', mt: 3, mb: 3 }}>
-          Current Employees
-        </Typography>
-
-        <DataGrid
-          rows={rowsConstructor(employeeProfile)}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          getRowId={(row, index) => `${index}-${row.firstName}-${row.lastName}-(${row.state})`}
-          slots={{ toolbar: GridToolbar }}
-          slotProps={{
-            toolbar: {
-              showQuickFilter: true,
-            },
-          }}
-          sx={{
-            boxShadow: '0 0px 8px 0  rgb(209, 212, 212);',
-            textAlign: 'center',
-            background: 'rgba(238, 239, 239, 0.62)',
-          }}
-        />
-      </Box>
-    </main>
-  );
-}
+         <DataGrid
+           rows={rowsConstructor(employeeProfile)}
+           columns={columns}
+           initialState={{
+             pagination: {
+               paginationModel: {
+                 pageSize: 5,
+               },
+             },
+           }}
+           pageSizeOptions={[5]}
+           getRowId={(row) => `${row.id})`}
+           slots={{ toolbar: GridToolbar }}
+           slotProps={{
+             toolbar: {
+               showQuickFilter: true,
+             },
+           }}
+           sx={{
+             boxShadow: '0 0px 8px 0  rgb(209, 212, 212);',
+             textAlign: 'center',
+             background: 'rgba(238, 239, 239, 0.62)',
+           }}
+         />
+       </Box>
+     </main>
+   );
+ }
